49a52459da8bf4519a0ab9ad0c31863ccce32be0
chrkhl <kuehlchristian@icloud.com>
2019-01-12 20:41:20 +0100

    supports more toy types (BB9E, R2D2, LightningMcQueen)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const { Scanner, Utils } = require('spherov2.js');
+const { Scanner } = require('spherov2.js');
 const colorConvert = require('color-convert');
+
+const supportedToys = [ 'SpheroMini', 'BB9E', 'LightningMcQueen', 'R2D2' ];
+
+const isToySupported = toyType => {
+  if (typeof toyType !== 'string') {
+    return false;
+  }
+
+  return supportedToys.includes(toyType);
+};
+
+const isToyNameSupported = toyName => typeof toyName === 'string' && toyName.length > 0;
 
 const createConnectorApi = toy => ({
   wake: () => {
@@ -10,6 +22,7 @@ const createConnectorApi = toy => ({
   },
   setMainLedColor: hexColor => {
     const [ red, green, blue ] = colorConvert.hex.rgb(hexColor);
+
     toy.wake();
     toy.setMainLedColor(red, green, blue);
   }
@@ -17,15 +30,61 @@ const createConnectorApi = toy => ({
 
 const connectSpheroMini = async () => {
   const toy = await Scanner.findSpheroMini();
+
   return toy ? createConnectorApi(toy) : null;
 };
 
 const connectSpheroMiniWithName = async name => {
   const toy = await Scanner.findSpheroMiniByName(name);
+
   return toy ? createConnectorApi(toy) : null;
+};
+
+const connectLightningMcQueen = async () => {
+  const toy = await Scanner.findLightningMcQueen();
+
+  return toy ? createConnectorApi(toy) : null;
+};
+
+const connectR2D2 = async () => {
+  const toy = await Scanner.findR2D2();
+
+  return toy ? createConnectorApi(toy) : null;
+};
+
+const connectBB9E = async () => {
+  const toy = await Scanner.findBB9E();
+
+  return toy ? createConnectorApi(toy) : null;
+};
+
+const connectToy = (toyType, toyName) => {
+  if (!isToySupported(toyType)) {
+    throw new Error(`Toys of type '${toyType}' not supported!`);
+  }
+
+  switch (toyType) {
+    case 'SpheroMini':
+      return isToyNameSupported(toyName) ?
+        connectSpheroMiniWithName(toyName) :
+        connectSpheroMini();
+    case 'BB9E':
+      return connectBB9E();
+    case 'LightningMcQueen':
+      return connectLightningMcQueen();
+    case 'R2D2':
+      return connectR2D2();
+    default:
+      return null;
+  }
 };
 
 module.exports = {
   connectSpheroMini,
-  connectSpheroMiniWithName
+  connectSpheroMiniWithName,
+  connectLightningMcQueen,
+  connectR2D2,
+  connectBB9E,
+  connectToy,
+  isToySupported
 };
